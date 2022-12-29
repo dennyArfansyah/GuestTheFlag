60e0b7ce1db27589dca47bd9ea7759f4d28f85a9
dennyArfansyah <dennyarfansyah@gmail.com>
2022-12-29 09:48:45 +0700

    Initial Commit

---
+++ GuestTheFlag/GuestTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuestTheFlagApp.swift
+//  GuestTheFlag
+//
+//  Created by Denny Arfansyah on 29/12/22.
+//
+
+import SwiftUI
+
+@main
+struct GuestTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
